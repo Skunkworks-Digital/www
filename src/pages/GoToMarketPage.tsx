8e7a05b91d823a9a70332d0a6f3a2bd377a286d5
Selaelo2 <selaelolanga10@gmail.com>
2025-05-27 15:55:59 +0200

    Create GoToMarketPage.tsx

---
+++ src/pages/GoToMarketPage.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import GoToMarket from "../components/GoToMarket";
+
+const GoToMarketPage: React.FC = () => {
+  return <GoToMarket />;
+};
+
+export default GoToMarketPage;
